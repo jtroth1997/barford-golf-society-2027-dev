574865f14fac6e7d6522dbc885709be5b707d47a
jtroth1997 <troth.jack@yahoo.co.uk>
2026-07-27 17:57:37 +0100

    Cache payment beta assets

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "barford-golf-rapid-v3";
+const CACHE_NAME = "barford-golf-rapid-v4";
 const CORE = [
-  "./","./index.html","./events.html","./scores.html","./gallery.html","./account.html","./worldevents.html","./shop.html","./about.html",
-  "./assets/css/styles.css?v=fast3","./assets/css/accessible-mobile.css?v=rapid1","./assets/css/events.css","./assets/css/scores.css?v=7","./assets/css/members.css","./assets/css/gallery.css",
-  "./assets/js/app.js?v=rapid5","./assets/js/accessible-mobile.js?v=rapid2","./assets/js/member-session.js","./assets/js/scores.js?v=7","./assets/js/scores-data.js","./assets/js/handicap-engine.js",
+  "./","./index.html","./payment-beta.html","./events.html","./scores.html","./gallery.html","./account.html","./worldevents.html","./shop.html","./about.html",
+  "./assets/css/styles.css?v=fast3","./assets/css/payment-beta.css?v=1","./assets/css/payment-beta-shared.css?v=beta1","./assets/css/accessible-mobile.css?v=rapid1","./assets/css/events.css","./assets/css/scores.css?v=7","./assets/css/members.css","./assets/css/gallery.css",
+  "./assets/js/app.js?v=rapid6","./assets/js/payment-beta.js?v=1","./assets/js/payment-beta-shared.js?v=beta1","./assets/js/accessible-mobile.js?v=rapid2","./assets/js/member-session.js","./assets/js/scores.js?v=7","./assets/js/scores-data.js","./assets/js/handicap-engine.js",
   "./assets/images/barford-golf-society-logo.png"
 ];
 self.addEventListener("install", event => {
