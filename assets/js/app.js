7840b77204e02061c475e5bd3852e623222b4ee4
jtroth1997 <troth.jack@yahoo.co.uk>
2026-07-27 17:57:32 +0100

    Refresh service worker for payment beta

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@
 const body = document.body;
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js?v=rapid3").catch(() => {});
+    navigator.serviceWorker.register("./sw.js?v=rapid4").catch(() => {});
   }, { once:true });
 }
 document.querySelectorAll(".menu-button").forEach(menuButton => {
