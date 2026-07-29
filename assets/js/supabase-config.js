597071e2402a8871850324be66e410a4d1046a2f
jtroth1997 <troth.jack@yahoo.co.uk>
2026-07-29 12:47:35 +0100

    Configure isolated 2027 Supabase foundation

---
+++ assets/js/supabase-config.js
@@ -0,0 +1,7 @@
+window.BARFORD_2027_CONFIG = Object.freeze({
+  supabaseUrl: "https://xspzmthygrajzktydvvj.supabase.co",
+  supabasePublishableKey: "sb_publishable_xLM39PjQf4XdTVfNHFOzAQ_i4re6w_c",
+  season: 2027,
+  galleryBucket: "gallery-images",
+  profileBucket: "profile-images"
+});
