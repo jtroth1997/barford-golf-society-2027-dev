aafdb1f708d7d0bf93a7d694ff79bd061b2b0ecd
jtroth1997 <troth.jack@yahoo.co.uk>
2026-07-29 09:58:50 +0100

    Mobile redesign: sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "barford-golf-rapid-v15";
+const CACHE_NAME = "barford-golf-mobile-v3";
 const CORE = [
   "./","./index.html","./events.html","./scores.html","./gallery.html","./account.html","./worldevents.html","./shop.html","./about.html","./admin.html","./signup.html","./payment-beta.html",
-  "./assets/css/styles.css?v=fast3","./assets/css/accessible-mobile.css?v=rapid2","./assets/css/events.css","./assets/css/tee-organiser.css","./assets/css/scores.css?v=7","./assets/css/mobile-menu-fix.css?v=1","./assets/css/members.css","./assets/css/gallery.css","./assets/css/admin.css","./assets/css/payment-beta.css?v=3","./assets/css/payment-beta-shared.css?v=beta4",
-  "./assets/js/app.js?v=rapid17","./assets/js/accessible-mobile.js?v=rapid2","./assets/js/member-session.js","./assets/js/event-countdown.js","./assets/js/event-weather.js","./assets/js/event-camera.js","./assets/js/admin.js","./assets/js/scores.js?v=7","./assets/js/scores-data.js","./assets/js/handicap-engine.js","./assets/js/payment-beta.js?v=4","./assets/js/payment-beta-shared.js?v=beta11",
+  "./assets/css/styles.css?v=fast3","./assets/css/accessible-mobile.css?v=mobile3","./assets/css/events.css","./assets/css/tee-organiser.css","./assets/css/scores.css?v=7","./assets/css/mobile-menu-fix.css?v=1","./assets/css/members.css","./assets/css/gallery.css","./assets/css/admin.css","./assets/css/payment-beta.css?v=3","./assets/css/payment-beta-shared.css?v=beta4",
+  "./assets/js/app.js?v=mobile3","./assets/js/accessible-mobile.js?v=mobile3","./assets/js/member-session.js","./assets/js/event-countdown.js","./assets/js/event-weather.js","./assets/js/event-camera.js","./assets/js/admin.js","./assets/js/scores.js?v=7","./assets/js/scores-data.js","./assets/js/handicap-engine.js","./assets/js/payment-beta.js?v=4","./assets/js/payment-beta-shared.js?v=beta11",
   "./assets/images/barford-golf-society-logo.png"
 ];
 
