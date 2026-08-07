8493e2e04240cded54165b33c0f337981b6d2326
jtroth1997 <troth.jack@yahoo.co.uk>
2026-08-07 14:35:13 +0100

    Refresh homepage asset cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "barford-golf-2027-speed-v35";
+const CACHE = "barford-golf-2027-speed-v36";
 const CORE = [
   "./",
   "./index.html",
@@ -18,11 +18,14 @@ const CORE = [
   "./assets/css/styles.css?v=homephotos3",
   "./assets/css/styles.css?v=homephotos4",
   "./assets/css/styles.css?v=homephotos5",
+  "./assets/css/styles.css?v=homephotos6",
   "./assets/css/members.css?v=rsvp1",
   "./assets/css/members.css?v=teeclick2",
   "./assets/css/members.css?v=account5",
   "./assets/css/members.css?v=cancel1",
+  "./assets/css/members.css?v=homefit1",
   "./assets/css/accessible-mobile.css?v=mobile6",
+  "./assets/css/accessible-mobile.css?v=homefit1",
   "./assets/css/events.css?v=video1",
   "./assets/css/events.css?v=rsvpmanage1",
   "./assets/css/scores.css?v=league6",
@@ -49,6 +52,7 @@ const CORE = [
   "./assets/js/home-photos.js?v=1",
   "./assets/js/home-photos.js?v=2",
   "./assets/js/home-photos.js?v=3",
+  "./assets/js/home-photos.js?v=4",
   "./assets/js/accessible-mobile.js?v=mobile6",
   "./assets/js/supabase-config.js?v=live1",
   "./assets/js/supabase-client.js?v=live1",
