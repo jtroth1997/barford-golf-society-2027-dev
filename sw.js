7a275a6200ca89af30fc9583204215376650ca79
jtroth1997 <troth.jack@yahoo.co.uk>
2026-08-17 15:27:12 +0100

    Fit score entry into one phone screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "barford-golf-2027-live-card-v1";
+const CACHE = "barford-golf-2027-no-scroll-v1";
 const CORE = [
   "./",
   "./index.html",
@@ -44,6 +44,7 @@ const CORE = [
   "./assets/css/scoring.css?v=1",
   "./assets/css/scoring-simple.css?v=1",
   "./assets/css/scoring-simple.css?v=livecard1",
+  "./assets/css/scoring-simple.css?v=noscr1",
   "./assets/css/admin-scoring-results.css?v=1",
   "./assets/css/admin-scoring-results.css?v=endround1",
   "./assets/css/score-competitions.css?v=1",
@@ -79,6 +80,7 @@ const CORE = [
   "./assets/js/scoring.js?v=trust3",
   "./assets/js/scoring.js?v=cardreview1",
   "./assets/js/scoring.js?v=livecard1",
+  "./assets/js/scoring.js?v=noscr1",
   "./assets/js/gallery-live.js?v=swipe1",
   "./assets/js/home-photos.js?v=1",
   "./assets/js/home-photos.js?v=2",
