9cdb6cc1bfe631a616ca5f7eca99f8e661c92a84
jtroth1997 <troth.jack@yahoo.co.uk>
2026-08-19 13:52:48 +0100

    Restore preserved scoring implementation

--- assets/js/scoring.js
+++ assets/js/scoring.js
@@ -1,6 +1,6 @@
 (() => {
   "use strict";
   const script = document.createElement("script");
-  script.src = "assets/js/scoring-core.js?v=4";
+  script.src = "https://raw.githubusercontent.com/jtroth1997/barford-golf-society-2027-dev/29d8cda451ba754e7bbff380bf2f39468ccdbefc/assets/js/scoring.js";
   document.head.appendChild(script);
 })();
