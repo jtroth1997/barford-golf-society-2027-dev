12c2a6b964c9c072857fb3fce40e356d00cc53ab
jtroth1997 <troth.jack@yahoo.co.uk>
2026-08-20 21:12:46 +0100

    Refresh cached admin update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "barford-golf-2027-live-first-v24";
+const CACHE = "barford-golf-2027-live-first-v25";
 const OFFLINE_PAGES = [
   "./", "./index.html", "./events.html", "./scores.html", "./gallery.html",
   "./account.html", "./payments.html", "./scoring.html", "./admin.html"
