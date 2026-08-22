0a1b7ae2f15be9d53d27e2dd1977a34e223d37ee
jtroth1997 <troth.jack@yahoo.co.uk>
2026-08-22 23:15:38 +0100

    Remove legacy automatic course finder

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "barford-golf-2027-live-first-v27";
+const CACHE = "barford-golf-2027-live-first-v28";
 const OFFLINE_PAGES = [
   "./", "./index.html", "./events.html", "./scores.html", "./gallery.html",
   "./account.html", "./payments.html", "./scoring.html", "./admin.html"
