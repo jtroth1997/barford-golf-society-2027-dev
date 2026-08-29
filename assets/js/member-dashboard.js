6925882b77c13d59d9e1e5715e4f5f823559263c
jtroth1997 <troth.jack@yahoo.co.uk>
2026-08-29 10:14:38 +0100

    Remove obsolete locked-state player list handler

--- assets/js/member-dashboard.js
+++ assets/js/member-dashboard.js
@@ -546,7 +546,6 @@
     })
   );
   document.getElementById("dashboardSeeSignups")?.addEventListener("click", loadPlayingList);
-  document.getElementById("dashboardLockedSeePlayers")?.addEventListener("click", loadPlayingList);
   document.getElementById("dashboardEventDirections")?.addEventListener("click", openDirections);
   document.getElementById("dashboardRsvpClose")?.addEventListener("click", () => rsvpDialog?.close());
   document.getElementById("dashboardRsvpCancel")?.addEventListener("click", () => rsvpDialog?.close());
